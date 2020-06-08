8b65b8c247e6d7653fbda667a647b8feaf99acdb
umang <iumangsharma001@gmail.com>
2020-06-08 14:18:03 +0530

    Buttons added.

---
+++ app/src/main/java/com/sharmaumang/truthdare/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sharmaumang.truthdare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button mButton;
+    ImageView mImageView;
+    Random mRandom = new Random();
+    int lastposition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Spin (View view){
+
+        mRandom =
+
+    }
+
+}
