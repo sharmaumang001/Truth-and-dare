c01d50cad516ff51a95652f7f40bf56c184a6815
umang <iumangsharma001@gmail.com>
2020-06-08 16:54:01 +0530

    Spin animation added and Full app working.

--- app/src/main/java/com/sharmaumang/truthdare/MainActivity.java
+++ app/src/main/java/com/sharmaumang/truthdare/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -14,17 +16,50 @@ public class MainActivity extends AppCompatActivity {
     Button mButton;
     ImageView mImageView;
     Random mRandom = new Random();
-    int lastposition;
+    int lastPosition, newPosition;
+    float pivotX,pivotY;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView=findViewById(R.id.imageView);
+        mButton=findViewById(R.id.button);
     }
 
     public void Spin (View view){
 
-        mRandom =
+        newPosition = mRandom.nextInt(36000);
+
+        pivotX = mImageView.getPivotX();
+        pivotY = mImageView.getPivotY();
+
+        Animation rotate = new RotateAnimation(lastPosition,newPosition, pivotX,pivotY);
+        rotate.setDuration(2500);
+        rotate.setFillAfter(true);
+
+        rotate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                mButton.setEnabled(false);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mButton.setEnabled(true);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        lastPosition=newPosition;
+
+        mImageView.startAnimation(rotate);
+
 
     }
 
